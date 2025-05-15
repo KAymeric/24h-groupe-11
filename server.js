359eb18202f40a51400c01b4394e34a748002812
kaymeric <aymeric.kreuzer@ynov.com>
2025-05-15 16:14:15 +0200

    add database connexion

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 require('dotenv').config();
 const app = require('./src/app');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const db = require('./src/database/connect');
 
 const PORT = process.env.PORT || 5180;
 
-app.listen(PORT, () => {
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html', { root: __dirname });
+});
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+})
+
+server.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${"http://localhost:" + PORT}`);
 });
